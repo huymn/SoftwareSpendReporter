96a7e738d0b320ad7abaa91c091854ceb263dfce
nguyenhu21 <35584482+nguyenhu21@users.noreply.github.com>
2021-05-14 16:41:20 -0700

    initial commit

---
+++ src/Transaction.java
@@ -0,0 +1,16 @@
+public class Transaction {
+    private String transactionDate;
+    private String vendor;
+    private String product;
+    private int amount;
+
+    public Transaction(String td, String v, String p, int a) {
+        this.transactionDate = td;
+        this.vendor = v;
+        this.product = p;
+        this.amount = a;
+    }
+
+    //Accessors
+
+}
